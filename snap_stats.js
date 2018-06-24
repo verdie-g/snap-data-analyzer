d1f850c06554bed4a1bd53dbc566a8df3bb0fa19
Grégoire <gregoire.verdier@gmail.com>
2018-06-24 13:10:12 +0200

    Add messages stats

--- snap_stats.js
+++ snap_stats.js
@@ -26,24 +26,56 @@ function countSnaps(snaps, snapsByUser, userField) {
   });
 }
 
+function getSnaps(jsonFolder) {
+  const snaps = readJsonFile(`${jsonFolder}/snap_history.json`);
+
+  const snapsByUser = {};
+  countSnaps(snaps['Received Snap History'], snapsByUser, 'From');
+  countSnaps(snaps['Sent Snap History'], snapsByUser, 'To');
+
+  const snapUsers = Object.keys(snapsByUser);
+  const totalSnaps = Object.keys(snapsByUser).map(user => snapsByUser[user].total);
+
+  return { snapUsers, totalSnaps };
+}
+
+function countMessages(messages, messagesByUser, userField) {
+  messages.forEach(message => {
+    if (messagesByUser[message[userField]] === undefined) {
+      messagesByUser[message[userField]] = 0;
+    }
+    messagesByUser[message[userField]] += 1;
+  });
+}
+
+function getMessages(jsonFolder) {
+  const chat = readJsonFile(`${jsonFolder}/chat_history.json`);
+
+  const messagesByUser = {};
+  countMessages(chat['Received Chat History'], messagesByUser, 'From');
+  countMessages(chat['Sent Chat History'], messagesByUser, 'To');
+
+  const chatUsers = Object.keys(messagesByUser);
+  const messagesCount = Object.keys(messagesByUser).map(user => messagesByUser[user]);
+
+  return { chatUsers, messagesCount };
+}
+
+function generateStatsFile(ctx) {
+  const template = fs.readFileSync(templateName).toString();
+  Object.keys(ctx).forEach(key => { ctx[key] = JSON.stringify(ctx[key]) });
+  const rendered = mustache.render(template, ctx);
+  fs.writeFileSync('index.html', rendered);
+}
+
 if (process.argv.length <= 2) {
   console.error(`usage: node snap_graph.js {json_folder}`);
   process.exit(1);
 }
 
 const jsonFolder = process.argv[2];
-const snaps = readJsonFile(`${jsonFolder}/snap_history.json`);
 
-const snapsByUser = {};
-countSnaps(snaps['Received Snap History'], snapsByUser, 'From');
-countSnaps(snaps['Sent Snap History'], snapsByUser, 'To');
-
-const users = Object.keys(snapsByUser);
-const totalSnaps = Object.keys(snapsByUser).map(user => snapsByUser[user].total);
-
-const ctx = { users, totalSnaps };
-
-const template = fs.readFileSync(templateName).toString();
-Object.keys(ctx).forEach(key => { ctx[key] = JSON.stringify(ctx[key]) });
-const rendered = mustache.render(template, ctx);
-fs.writeFileSync("index.html", rendered);
+generateStatsFile({
+  ...getSnaps(jsonFolder),
+  ...getMessages(jsonFolder),
+});
